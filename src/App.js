19f1af708de878b6792906d96c292680f7fe205f
William Gomes <WilliamPg1998@gmail.com>
2021-06-19 14:21:04 -0400

    Added some features

--- src/App.js
+++ src/App.js
@@ -3,17 +3,24 @@ import Shop from './Shop.js'
 import { useState } from 'react';
 
 function App() {
+  const [distance, setDistance] = useState(0)
   const [points, setPoints] = useState(0)
   const [multiplier, setMultiplier] = useState(1)
 
+  
+
   function clickTire() {
     setPoints(points + multiplier)
+    setDistance(distance + multiplier)
+    // Spin the wheel: (Shop Item)
+    // let wheel = document.getElementsByClassName('tireImage')
+    // wheel[0].style.animation = `rotation ${points}s infinite linear`
   }
 
   return (
     <div>
       <h1>Race Clicker</h1>
-      <p>{points}</p>
+      <p>Speed:{points}km/h</p>
       <div className="tireImage" onClick={() => { clickTire() }}></div>
       <Shop 
       points={points}
@@ -21,6 +28,7 @@ function App() {
       multiplier={multiplier}
       setMultiplier={setMultiplier}
       />
+      <p className="distance">Distance Traveled:{distance}</p>
     </div>
   );
 }
--- src/Shop.js
+++ src/Shop.js
@@ -1,22 +1,40 @@
 import { useState } from 'react'
 const Shop = ({ points, setPoints, multiplier, setMultiplier }) => {
 
-    const [cost, setCost] = useState(50)
+  const [doubleCost, setDoubleCost] = useState(50)
+  const [firstPassiveCost, setFirstPassiveCost] = useState(5)
 
-    function doubleMultiplier() {
-        if (points >= cost) {
-            setCost(Math.floor(cost * 1.5))
-            setMultiplier(multiplier + 1);
-            setPoints(points - cost)
-        }
+  function doubleMultiplier() {
+    if (points >= doubleCost) {
+      setDoubleCost(Math.floor(doubleCost * 2.5))
+      setMultiplier(multiplier + 1)
+      setPoints(points - doubleCost)
     }
-    return (
-        <main className="Shop">
-            <div className="buttonCost"></div>
-            <p>Double your points!</p>
-            <button onClick={() => { doubleMultiplier() }}>Cost: {cost}</button>
-        </main>
-    )
+  }
+
+  function firstPassive() {
+    if (points >= firstPassiveCost) {
+      setFirstPassiveCost(Math.floor(firstPassiveCost * 2.5))
+      setPoints(points - firstPassiveCost)
+      setInterval(() => { setPoints(points + 1) }, 1000) 
+    }
+    
+  }
+
+  return (
+    <div>
+      <section className="double shopSelection">
+        <div className="buttonCost"></div>
+        <p>Add one to your clicks!</p>
+        <button onClick={() => { doubleMultiplier() }}>Cost: {doubleCost}</button>
+      </section>
+      <section className="firstPassive shopSelection">
+        <div className="buttonCost"></div>
+        <p>Passively gain speed!</p>
+        <button onClick={() => { firstPassive() }}>Cost: {firstPassiveCost}</button>
+      </section>
+    </div>
+  )
 }
 
 export default Shop
